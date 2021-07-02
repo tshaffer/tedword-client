04782a0e3f849dc37c605b2147e437452e1aef36
Ted Shaffer <ted@brightsign.biz>
2021-07-02 07:12:24 -0700

    Display puzzle using npm package.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import { isEmpty } from 'lodash';
+
 import { DisplayedPuzzle, FileInput } from '../types';
 import { loadPuzzle } from '../controllers';
 import { getDisplayedPuzzle } from '../selectors';
+
+// import Crossword from '@jaredreisinger/react-crossword';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const rc = require('@jaredreisinger/react-crossword');
+console.log(rc);
+const Crossword = require('@jaredreisinger/react-crossword').Crossword;
+console.log(Crossword);
 
 export interface HomeProps {
   displayedPuzzle: DisplayedPuzzle;
@@ -22,17 +32,33 @@ const Home = (props: HomeProps) => {
 
   console.log(props.displayedPuzzle);
 
+  console.log(isEmpty(props.displayedPuzzle.across));
+  console.log(isEmpty(props.displayedPuzzle.down));
+  console.log(Object.keys(props.displayedPuzzle.across).length);
+  console.log(Object.keys(props.displayedPuzzle.down).length);
+
+  if (isEmpty(props.displayedPuzzle.across) && isEmpty(props.displayedPuzzle.down)) {
+    return (
+      <div>
+        <p>
+          Uncooked Pizza
+        </p>
+        <input
+          type="file"
+          id="fileInput"
+          onChange={handleSelectPuzzle}
+        >
+        </input>
+      </div>
+    );
+  }
+
   return (
     <div>
       <p>
-        Pizza
+        Cooked Pizza
       </p>
-      <input
-        type="file"
-        id="fileInput"
-        onChange={handleSelectPuzzle}
-      >
-      </input>
+      <Crossword data={props.displayedPuzzle} />
     </div>
   );
 
