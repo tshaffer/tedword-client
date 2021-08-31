90751b1e174fffba0c98e2c648c573cd356a33ca
Ted Shaffer <ted@brightsign.biz>
2021-08-31 08:17:04 -0700

    pull out FileUpload

--- src/components/GameHome.tsx
+++ src/components/GameHome.tsx
@@ -3,26 +3,21 @@ import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { AppState, UiState, PuzzlesMetadataMap, PuzzleMetadata, BoardEntity, BoardsMap, PuzzleExistsByFileNameMap } from '../types';
-import { getAppState, getBoards, getCurrentUser, getPuzzlesMetadata, getPuzzleExistsByFileNameMap } from '../selectors';
+import { UiState, PuzzleMetadata, BoardEntity } from '../types';
+import { getCurrentUser } from '../selectors';
 import { setBoardId, setPuzzleId, setUiState, setFileUploadStatus } from '../models';
 import {
   addUserToExistingBoard,
   createBoard,
   updateLastPlayedDateTime,
-  uploadPuzFiles,
 } from '../controllers';
 
 import NewGames from './NewGames';
 import ExistingGames from './ExistingGames';
-import { isNil } from 'lodash';
+import PuzzleUpload from './PuzzleUpload';
 
 export interface GameHomeProps {
-  appState: AppState,
-  boardsMap: BoardsMap;
   currentUser: string;
-  puzzlesMetadata: PuzzlesMetadataMap;
-  puzzleExistsByFileName: PuzzleExistsByFileNameMap;
   onAddUserToBoard: (id: string, userName: string) => any;
   onCreateBoard: () => any;
   onSetBoardId: (boardId: string) => any;
@@ -30,12 +25,9 @@ export interface GameHomeProps {
   onSetPuzzleId: (puzzleId: string) => any;
   onSetUiState: (uiState: UiState) => any;
   onUpdateLastPlayedDateTime: (boardId: string, dt: Date) => any;
-  onUploadPuzFiles: (files: File[]) => any;
 }
 
 const GameHome = (props: GameHomeProps) => {
-
-  const [files, setFiles] = React.useState<File[]>([]);
 
   const userInGame = (boardEntity: BoardEntity): boolean => {
     return boardEntity.users.includes(props.currentUser);
@@ -82,43 +74,6 @@ const GameHome = (props: GameHomeProps) => {
       transition: '0.3s'
     };
 
-    const displayNone = {
-      display: 'none',
-    };
-
-    const padded = {
-      margin: '4px',
-    };
-
-    const handleUploadPuzFiles = () => {
-      props.onSetFileUploadStatus('Uploading files...');
-      props.onUploadPuzFiles(files);
-    };
-
-    const handleDisplayFileSelect = () => {
-      fileSelectRef.current.click();
-    };
-
-    const handleSelectPuzFiles = (e: { target: { files: string | any[] | FileList; value: string; }; }) => {
-      if (!isNil(e.target.files)
-        && e.target.files.length > 0) {
-        const targetFileList: FileList = e.target.files as FileList;
-        const filesToAdd = [];
-        for (let i = 0; i < targetFileList.length; i++) {
-          const targetFile: File = e.target.files[i];
-          filesToAdd.push(targetFile);
-        }
-        setFiles(filesToAdd);
-      }
-      e.target.value = '';
-
-      if (e.target.files.length > 0) {
-        props.onSetFileUploadStatus('Upload pending...');
-      } else {
-        props.onSetFileUploadStatus('');
-      }
-    };
-
     function handleSelectTab(evt: any) {
 
       const selectedTabId = evt.target.id;
@@ -154,147 +109,12 @@ const GameHome = (props: GameHomeProps) => {
       }
     }
 
-    const getFilesLists = () => {
-
-      const existingFiles: string[] = [];
-      const newFiles: string[] = [];
-
-      for (const file of files) {
-        // eslint-disable-next-line no-prototype-builtins
-        if (props.puzzleExistsByFileName.hasOwnProperty(file.name)) {
-          existingFiles.push(file.name);
-        } else {
-          newFiles.push(file.name);
-        }
-      }
-
-      return {
-        newFiles,
-        existingFiles,
-      };
-    };
-
-    const renderUploadButton = (newFiles: any[]) => {
-
-      return newFiles.length === 0
-        ? null
-        : (
-          <div>
-            <p>
-              <button
-                type='button'
-                style={padded}
-                onClick={handleUploadPuzFiles}
-              >
-                Upload Files
-              </button>
-            </p>
-          </div>
-        );
-    };
-
-    const getNewFilesList = (newFiles: any[]) => {
-
-      if (newFiles.length === 0) {
-        return null;
-      }
-      const newFilesListItems = newFiles.map((newFile) =>
-        <li key={newFile}>{newFile}</li>
-      );
-      return (
-        <div>
-          <p>New files</p>
-          <ul>{newFilesListItems}</ul>
-        </div>
-      );
-    };
-
-    const getExistingFilesList = (existingFiles: any[]) => {
-
-      if (existingFiles.length === 0) {
-        return null;
-      }
-      const existingFilesListItems = existingFiles.map((existingFile) =>
-        <li key={existingFile}>{existingFile}</li>
-      );
-      return (
-        <div>
-          <p>Existing files (will not be uploaded)</p>
-          <ul>{existingFilesListItems}</ul>
-        </div>
-      );
-    };
-
-    const renderFilesList = () => {
-
-      if (files.length == 0) {
-        return (
-          <div>
-            <p>No files chosen</p>
-          </div>
-        );
-      }
-
-      const filesLists = getFilesLists();
-      const { existingFiles, newFiles } = filesLists;
-
-      const uploadButton = renderUploadButton(newFiles);
-
-      const newFilesList = getNewFilesList(newFiles);
-      const existingFilesList = getExistingFilesList(existingFiles);
-
-      return (
-        <div>
-          {newFilesList}
-          {existingFilesList}
-          {uploadButton}
-        </div>
-      );
-    };
-
-    const renderSettingsTab = () => {
-
-      const filesList = renderFilesList();
-
-      return (
-        <div id='settingsContent' style={tabcontent} ref={settingsContentRef}>
-          <div>
-            <input
-              type="file"
-              id="fileElem"
-              multiple
-              style={displayNone}
-              ref={fileSelectRef}
-              onChange={handleSelectPuzFiles}
-            />
-            <button
-              id="fileSelect"
-              style={padded}
-              onClick={handleDisplayFileSelect}
-            >
-              Choose Files
-            </button>
-          </div>
-          <div>
-            {filesList}
-          </div>
-          <div>
-            <p>
-              {props.appState.fileUploadStatus}
-            </p>
-          </div>
-        </div>
-      );
-    };
-
     const newGameTabSelectRef = React.createRef<any>();
     const newGamesContentRef = React.createRef<any>();
     const inProgressGamesTabSelectRef = React.createRef<any>();
     const inProgressGamesContentRef = React.createRef<any>();
     const settingsTabSelectRef = React.createRef<any>();
     const settingsContentRef = React.createRef<any>();
-
-    const fileSelectRef = React.createRef<any>();
 
     return (
       <div>
@@ -313,7 +133,9 @@ const GameHome = (props: GameHomeProps) => {
             onSelectBoard={handleOpenBoard}
           />
         </div>
-        {renderSettingsTab()}
+        <div id='settingsContent' style={tabcontent} ref={settingsContentRef}>
+          <PuzzleUpload />
+        </div>
       </div>
     );
   };
@@ -323,11 +145,7 @@ const GameHome = (props: GameHomeProps) => {
 
 function mapStateToProps(state: any) {
   return {
-    appState: getAppState(state),
-    boardsMap: getBoards(state),
     currentUser: getCurrentUser(state),
-    puzzlesMetadata: getPuzzlesMetadata(state),
-    puzzleExistsByFileName: getPuzzleExistsByFileNameMap(state),
   };
 }
 
@@ -340,7 +158,6 @@ const mapDispatchToProps = (dispatch: any) => {
     onSetPuzzleId: setPuzzleId,
     onSetUiState: setUiState,
     onUpdateLastPlayedDateTime: updateLastPlayedDateTime,
-    onUploadPuzFiles: uploadPuzFiles,
   }, dispatch);
 };
 
---
+++ src/components/PuzzleUpload.tsx
@@ -0,0 +1,211 @@
+import * as React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { isNil } from 'lodash';
+
+import { AppState, PuzzleExistsByFileNameMap } from '../types';
+import { getAppState, getPuzzleExistsByFileNameMap } from '../selectors';
+import { setFileUploadStatus } from '../models';
+import {
+  uploadPuzFiles,
+} from '../controllers';
+
+export interface PuzzleUploadProps {
+  appState: AppState,
+  puzzleExistsByFileName: PuzzleExistsByFileNameMap;
+  onSetFileUploadStatus: (fileUploadState: string) => any;
+  onUploadPuzFiles: (files: File[]) => any;
+}
+
+const PuzzleUpload = (props: PuzzleUploadProps) => {
+
+  const [files, setFiles] = React.useState<File[]>([]);
+  const [newFiles, setNewFiles] = React.useState<File[]>([]);
+  const [existingFiles, setExistingFiles] = React.useState<File[]>([]);
+
+  React.useEffect(() => {
+    buildFilesLists();
+  }, [files]);
+
+  const padded = {
+    margin: '4px',
+  };
+
+  const displayNone = {
+    display: 'none',
+  };
+
+  const buildFilesLists = (): void => {
+
+    const existingFiles: File[] = [];
+    const newFiles: File[] = [];
+
+    for (const file of files) {
+      // eslint-disable-next-line no-prototype-builtins
+      if (props.puzzleExistsByFileName.hasOwnProperty(file.name)) {
+        existingFiles.push(file);
+      } else {
+        newFiles.push(file);
+      }
+    }
+
+    setExistingFiles(existingFiles);
+    setNewFiles(newFiles);
+  };
+
+  const getNewFilesList = () => {
+
+    if (newFiles.length === 0) {
+      return null;
+    }
+    const newFilesListItems = newFiles.map((newFile) =>
+      <li key={newFile.name}>{newFile.name}</li>
+    );
+    return (
+      <div>
+        <p>New files</p>
+        <ul>{newFilesListItems}</ul>
+      </div>
+    );
+  };
+
+  const getExistingFilesList = () => {
+
+    if (existingFiles.length === 0) {
+      return null;
+    }
+    const existingFilesListItems = existingFiles.map((existingFile) =>
+      <li key={existingFile.name}>{existingFile.name}</li>
+    );
+    return (
+      <div>
+        <p>Existing files (will not be uploaded)</p>
+        <ul>{existingFilesListItems}</ul>
+      </div>
+    );
+  };
+
+  const handleDisplayFileSelect = () => {
+    fileSelectRef.current.click();
+  };
+
+  const handleSelectPuzFiles = (e: { target: { files: string | any[] | FileList; value: string; }; }) => {
+    if (!isNil(e.target.files)
+      && e.target.files.length > 0) {
+      const targetFileList: FileList = e.target.files as FileList;
+      const filesToAdd = [];
+      for (let i = 0; i < targetFileList.length; i++) {
+        const targetFile: File = e.target.files[i];
+        filesToAdd.push(targetFile);
+      }
+      setFiles(filesToAdd);
+    }
+    e.target.value = '';
+
+    if (e.target.files.length > 0) {
+      props.onSetFileUploadStatus('Upload pending...');
+    } else {
+      props.onSetFileUploadStatus('');
+    }
+  };
+
+  const handleUploadPuzFiles = () => {
+    props.onSetFileUploadStatus('Uploading files...');
+    props.onUploadPuzFiles(files);
+  };
+
+  const renderUploadButton = (newFiles: any[]) => {
+
+    return newFiles.length === 0
+      ? null
+      : (
+        <div>
+          <p>
+            <button
+              type='button'
+              style={padded}
+              onClick={handleUploadPuzFiles}
+            >
+              Upload Files
+            </button>
+          </p>
+        </div>
+      );
+  };
+
+  const renderFilesList = () => {
+
+    if (files.length == 0) {
+      return (
+        <div>
+          <p>No files chosen</p>
+        </div>
+      );
+    }
+
+    const uploadButton = renderUploadButton(newFiles);
+
+    const newFilesList = getNewFilesList();
+    const existingFilesList = getExistingFilesList();
+
+    return (
+      <div>
+        {newFilesList}
+        {existingFilesList}
+        {uploadButton}
+      </div>
+    );
+  };
+
+  const fileSelectRef = React.createRef<any>();
+
+  const filesList = renderFilesList();
+
+  return (
+    <div>
+      <div>
+        <input
+          type="file"
+          id="fileElem"
+          multiple
+          style={displayNone}
+          ref={fileSelectRef}
+          onChange={handleSelectPuzFiles}
+        />
+        <button
+          id="fileSelect"
+          style={padded}
+          onClick={handleDisplayFileSelect}
+        >
+          Choose Files
+        </button>
+      </div>
+      <div>
+        {filesList}
+      </div>
+      <div>
+        <p>
+          {props.appState.fileUploadStatus}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+function mapStateToProps(state: any) {
+  return {
+    appState: getAppState(state),
+    puzzleExistsByFileName: getPuzzleExistsByFileNameMap(state),
+  };
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  return bindActionCreators({
+    onSetFileUploadStatus: setFileUploadStatus,
+    onUploadPuzFiles: uploadPuzFiles,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PuzzleUpload);
+
