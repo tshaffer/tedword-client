8e08e898a154e5f7c7981c9ee105f6089e7d9bf2
Ted Shaffer <ted@brightsign.biz>
2021-09-26 09:18:30 -0700

    Display User Entries for selected clue.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,28 +2,70 @@ import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { GameState} from '../types';
-import { getGameState } from '../selectors';
+import { ClueAtLocation, CluesByDirection, CluesByNumber, GameState, Guess, GuessesGrid } from '../types';
+import { getCrosswordClues, getGameState, getGuesses } from '../selectors';
 import { isNil } from 'lodash';
 
 export interface BoardProps {
   gameState: GameState,
+  cluesByDirection: CluesByDirection;
+  guesses: GuessesGrid;
 }
 
 const Board = (props: BoardProps) => {
 
+  const getUserEntry = (direction: string, clueNumber: string): string => {
+
+    if (isNil(props.cluesByDirection) || isNil(props.guesses)) {
+      return '';
+    }
+
+    const cluesByNumber: CluesByNumber = props.cluesByDirection[direction];
+    const clueAtLocation: ClueAtLocation = cluesByNumber[clueNumber];
+    const clueRow = clueAtLocation.row;
+    const clueColumn = clueAtLocation.col;
+    const clueAnswer = clueAtLocation.answer;
+    let userEntry = '';
+    if (direction === 'across') {
+      for (let j = 0; j < clueAnswer.length; j++) {
+        const guess: Guess = props.guesses[clueRow][clueColumn + j];
+        if (guess.value === '') {
+          userEntry += '_';
+        } else {
+          userEntry += guess.value;
+        }
+      }
+    } else {
+      for (let j = 0; j < clueAnswer.length; j++) {
+        const guess: Guess = props.guesses[clueRow + j][clueColumn];
+        if (guess.value === '') {
+          userEntry += '_';
+        } else {
+          userEntry += guess.value;
+        }
+      }
+    }
+    console.log('userEntry:', userEntry, userEntry.length, clueAnswer.length);
+
+    return userEntry;
+  };
+
   let acrossClue: string = '';
+  let acrossUserEntry: string = '';
   let downClue: string = '';
+  let downUserEntry: string = '';
 
   if (!isNil(props.gameState)) {
     if (!isNil(props.gameState.focusedAcrossClue)) {
       acrossClue = props.gameState.focusedAcrossClue.number.toString() + 'a (' + props.gameState.focusedAcrossClue.length.toString() + ')' + props.gameState.focusedAcrossClue.text;
+      acrossUserEntry = getUserEntry('across', props.gameState.focusedAcrossClue.number.toString());
     }
     if (!isNil(props.gameState.focusedDownClue)) {
       downClue = props.gameState.focusedDownClue.number.toString() + 'd (' + props.gameState.focusedDownClue.length.toString() + ')' + props.gameState.focusedDownClue.text;
+      downUserEntry = getUserEntry('down', props.gameState.focusedDownClue.number.toString());
     }
   }
-  
+
   if (acrossClue === '') {
     acrossClue = 'A bug that nothing is displayed here';
     downClue = 'Working on it, harder than I thought';
@@ -32,7 +74,9 @@ const Board = (props: BoardProps) => {
   return (
     <div>
       <p>{acrossClue}</p>
+      <p>{acrossUserEntry}</p>
       <p>{downClue}</p>
+      <p>{downUserEntry}</p>
     </div>
   );
 };
@@ -40,6 +84,8 @@ const Board = (props: BoardProps) => {
 function mapStateToProps(state: any) {
   return {
     gameState: getGameState(state),
+    cluesByDirection: getCrosswordClues(state),
+    guesses: getGuesses(state),
   };
 }
 
