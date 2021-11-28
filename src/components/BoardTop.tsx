31337e79c2d42738b22533f66a01a25c42c26772
Ted Shaffer <ted@brightsign.biz>
2021-11-28 06:04:01 -0800

    Added Home button.

--- src/components/BoardTop.tsx
+++ src/components/BoardTop.tsx
@@ -8,8 +8,8 @@ import Board from './Board';
 import BoardPlay from './BoardPlay';
 import BoardToolbar from './BoardToolbar';
 import { getAppState, getBoard, getPuzzlesMetadata, getDisplayedPuzzle, getCellContents, getPuzzle } from '../selectors';
-import { AppState, BoardEntity, CellContentsMap, DisplayedPuzzle, PuzzlesMetadataMap, PuzzleSpec } from '../types';
-import Chat from './Chat';
+import { AppState, BoardEntity, CellContentsMap, DisplayedPuzzle, PuzzlesMetadataMap, PuzzleSpec, UiState } from '../types';
+import { setUiState, } from '../models';
 
 export interface BoardTopProps {
   appState: AppState,
@@ -17,15 +17,23 @@ export interface BoardTopProps {
   displayedPuzzle: DisplayedPuzzle;
   puzzlesMetadata: PuzzlesMetadataMap;
   puzzleSpec: PuzzleSpec;
+  onSetUiState: (uiState: UiState) => any;
 }
 
 const BoardTop = (props: BoardTopProps) => {
 
   //       <Chat/>
 
+  const handleHome = () => {
+    console.log('handleHome');
+    props.onSetUiState(UiState.SelectPuzzleOrBoard);
+  };
+
   return (
     <div style={{ position: 'relative', height: '1080px' }}>
       <div style={{ maxHeight: '800px', overflow: 'auto' }}>
+        <button onClick={handleHome}>Home</button>
+        <br />
         <BoardToolbar />
         <Board />
         <BoardPlay
@@ -56,6 +64,7 @@ function mapStateToProps(state: any) {
 
 const mapDispatchToProps = (dispatch: any) => {
   return bindActionCreators({
+    onSetUiState: setUiState,
   }, dispatch);
 };
 
